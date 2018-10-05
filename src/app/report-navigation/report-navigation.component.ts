ce250a8b51e5097c393789d413c5203a89cc98cc
Arnab <arnab_majumdar2007@yahoo.co.in>
2018-10-05 11:44:13 +0530

    Style and dynamic left panel

--- src/app/report-navigation/report-navigation.component.ts
+++ src/app/report-navigation/report-navigation.component.ts
@@ -6,6 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report-navigation.component.css']
 })
 export class ReportNavigationComponent implements OnInit {
+  reportNavigation = [
+    {
+      title: 'Menu Item 1',
+      url: '/',
+      target: '_blank',
+      iconClass: 'fa-fw fa-dashboard'
+    },
+    {
+      title: 'Menu Item 2',
+      active: true,
+      iconClass: 'fa-fw fa-dashboard',
+      navigationItems: [
+        {
+          title: 'Sub-Menu Item 1',
+          url: '/',
+          iconClass: 'fa-fw fa-dashboard'
+        },
+        {
+          title: 'Sub-Menu Item 2',
+          url: '/',
+          iconClass: 'fa-fw fa-dashboard'
+        }
+      ]
+    }
+  ];
   constructor() { }
   ngOnInit() {
   }
